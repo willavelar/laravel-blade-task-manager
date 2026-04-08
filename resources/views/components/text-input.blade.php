f909c155ffd29e5ea6a6f87d66a1025df90e8492
William Avelar <avelar.developer@gmail.com>
2026-04-07 23:01:38 -0300

    feat: apply dark theme to auth views (login, register)

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-slate-600 bg-slate-700 text-slate-100 focus:border-violet-500 focus:ring-violet-500 rounded-md shadow-sm placeholder-slate-500']) }}>
